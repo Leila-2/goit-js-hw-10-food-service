4a9b5e88fc7221e9c2bf5a27852f5a38aaf883af
Leila-2 <liliya.yavorskaya17@gmail.com>
2021-08-31 17:04:59 +0300

    добавляет разметку и переключатель темы т

---
+++ src/js/index.js
@@ -0,0 +1,48 @@
+//IMPORTS
+import menu from './menu.json';
+import menuTemplate from '../templates/menu-template.hbs';
+
+//ДАННЫЕ
+const Theme = {
+    LIGHT: 'light-theme',
+    DARK: 'dark-theme',
+};
+
+
+
+//ДОСТУПЫ
+const body = document.querySelector('body');
+const list = document.querySelector('.js-menu');
+const changeTheme = document.getElementById('theme-switch-toggle');
+//по умолчанию
+body.classList.add(Theme.LIGHT)
+
+
+// СЛУШАТЕЛИ
+changeTheme.addEventListener('change', themeSwitcher)
+
+//THEME SWITCHER
+function themeSwitcher() {
+    if (body.classList.contains(Theme.LIGHT)) {
+        body.classList.remove(Theme.LIGHT)
+        localStorage.removeItem("theme", "LIGHT")
+        body.classList.add(Theme.DARK)
+        localStorage.setItem("theme", "DARK")
+
+        changeTheme.checked = true;
+    }
+
+    else {
+
+        body.classList.remove(Theme.DARK)
+        localStorage.removeItem("theme", "DARK")
+        body.classList.add(Theme.LIGHT)
+        localStorage.setItem("theme", "LIGHT")
+        //changeTheme.checked = false;
+    }
+
+}
+
+//MENU CREATOR
+const menuCreator = menuTemplate(menu);
+list.insertAdjacentHTML('beforeend', menuCreator)
